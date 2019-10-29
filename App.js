3b55f44e4c101fff11333ca97c88be854d380cf1
Selena <selena.rikley@gmail.com>
2019-10-28 23:13:58 -0400

    Move files to src folder

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import ErrorBoundary from './ErrorBoundary';
 import HomeScreen from './screens/HomeScreen';
@@ -27,8 +27,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
