bbf7c80c700ff69d485b4ee9538f52ee01bbcd25
kerianne-rikley <kerianne.rikley@gmail.com>
2019-11-03 14:31:28 -0500

    Moved entry states up to GameScreen, added a submit button

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,16 +1,42 @@
 import * as React from "react";
-import { StyleSheet, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 import Entry from "../components/Entry";
 
 export default class GameScreen extends React.Component {
+  state = {
+    codeHistory: [],
+    currentChoices: [0,0,0,0],
+  }
+
+  choices = [0,1,2,3,4,5];
+
+  updateChoice = (buttonId) => {
+    let newChoiceId = (this.state.currentChoices[buttonId] + 1) % 6;
+    const newChoices = this.state.currentChoices;
+    newChoices[buttonId] = newChoiceId;
+    this.setState(newChoices);
+  }
+
+  updateHistory = () => {
+    this.setState({codeHistory: [this.state.currentChoices]})
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.code}>
-          <Entry id={0} />
-          <Entry id={1} />
-          <Entry id={2} />
-          <Entry id={3} />
+        <View style={styles.container}>
+          <View style={styles.code}>
+            <Entry id={0} choice={this.choices[this.state.currentChoices[0]]} handlePress={this.updateChoice}/>
+            <Entry id={1} choice={this.choices[this.state.currentChoices[1]]} handlePress={this.updateChoice}/>
+            <Entry id={2} choice={this.choices[this.state.currentChoices[2]]} handlePress={this.updateChoice}/>
+            <Entry id={3} choice={this.choices[this.state.currentChoices[3]]} handlePress={this.updateChoice}/>
+            <Button
+              testID="submit-button"
+              title="Submit"
+              onPress={() => {this.updateHistory()}}
+            />
+          </View>
+          <Text>{this.state.codeHistory}</Text>
         </View>
         <Button
           testID="home-button"
@@ -35,7 +61,8 @@ const styles = StyleSheet.create({
     marginVertical: 10
   },
   code: {
-    flex: 1,
-    flexDirection: "row"
-  }
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: '20'
+  }   
 });
