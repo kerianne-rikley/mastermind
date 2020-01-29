6d629e7cb9e935e718d28bfae885e249b70d70cd
Selena <selena.rikley@gmail.com>
2020-01-28 20:21:45 -0500

    Merge rebase changes

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -4,7 +4,6 @@ import Entry from "../components/Entry";
 import Attempt from "../components/Attempt";
 
 export default class GameScreen extends React.Component {
-<<<<<<< HEAD
   state = {
     codeHistory: [[6,6,6,6],[6,6,6,6],[6,6,6,6],[6,6,6,6],[6,6,6,6],[6,6,6,6],[6,6,6,6],[6,6,6,6],[6,6,6,6],[6,6,6,6]],
     codeFeedback: [[6,6,6,6],[6,6,6,6],[6,6,6,6],[6,6,6,6],[6,6,6,6],[6,6,6,6],[6,6,6,6],[6,6,6,6],[6,6,6,6],[6,6,6,6]],
@@ -13,8 +12,6 @@ export default class GameScreen extends React.Component {
     attempts: 0
   }
 
-=======
->>>>>>> 62f8b6318203c5c11430e6f93cb7e89e401810f6
   choices = [0,1,2,3,4,5];
 
   constructor(){
@@ -79,7 +76,6 @@ export default class GameScreen extends React.Component {
       <View style={styles.container}>
         <View style={styles.container}>
           <View style={styles.code}>
-<<<<<<< HEAD
             <Entry id={0} choice={this.choices[this.state.currentChoices[0]]} handlePress={this.updateChoice}/>
             <Entry id={1} choice={this.choices[this.state.currentChoices[1]]} handlePress={this.updateChoice}/>
             <Entry id={2} choice={this.choices[this.state.currentChoices[2]]} handlePress={this.updateChoice}/>
@@ -97,27 +93,6 @@ export default class GameScreen extends React.Component {
           <Attempt code={this.state.codeHistory[8]} feedback={this.state.codeFeedback[8]} />
           <Attempt code={this.state.codeHistory[9]} feedback={this.state.codeFeedback[9]} />
           <Text>{this.state.attempts}</Text>
-=======
-            <Entry id={0} choice={this.choices[this.state.currentChoices[0]]} handlePress={this.updateChoice} />
-            <Entry id={1} choice={this.choices[this.state.currentChoices[1]]} handlePress={this.updateChoice} />
-            <Entry id={2} choice={this.choices[this.state.currentChoices[2]]} handlePress={this.updateChoice} />
-            <Entry id={3} choice={this.choices[this.state.currentChoices[3]]} handlePress={this.updateChoice} />
-            <Button
-              testID="submit-button"
-              title="Submit"
-              onPress={() => {this.updateHistory()}}
-            />
-          </View>
-          <View style={styles.history}>
-            <Attempt code={this.state.codeHistory[0]} />
-            <Attempt code={this.state.codeHistory[1]} />
-            <Attempt code={this.state.codeHistory[2]} />
-            <Attempt code={this.state.codeHistory[3]} />
-            <Attempt code={this.state.codeHistory[4]} />
-            <Attempt code={this.state.codeHistory[5]} />
-          {/* <Text>{this.state.codeHistory}</Text> */}
-          </View>
->>>>>>> 62f8b6318203c5c11430e6f93cb7e89e401810f6
         </View>
         <Button
           testID="home-button"
